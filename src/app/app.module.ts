498ddb4607902df31679a28cd05fbd63742dd0ba
villasana26enrique <enrique.villasana26@gmail.com>
2020-05-31 14:24:19 -0400

    Creando Componentes, Rutas y Formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './pages/template/template.component';
+import { ReactiveComponent } from './pages/reactive/reactive.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    ReactiveComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
