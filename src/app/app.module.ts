3b1e4c0d11d86588a20b318eba884deff69f55cc
villasana26enrique <enrique.villasana26@gmail.com>
2020-06-03 17:44:03 -0400

    Configurando la app para trabajar  con Forms Reactivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+/* FormsModule: para Utilizar Formularios por Template
+* ReactiveFormsModule: Para Utilizar Formularios Reactivos */
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +20,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
