bcde9115718e9a40bbdaf1ad6823bf6e1d831c9d
villasana26enrique <enrique.villasana26@gmail.com>
2020-06-06 21:48:57 -0400

    Eliminando import que no se utiliza

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
-import { UpperCasePipe } from '@angular/common';
 import { ValidatorsService } from '../../services/validators.service';
 
 @Component({
